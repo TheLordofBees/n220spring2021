bdaa191f4aafee8b67913d9a8bcd6c5a5a7c2a83
TheLordofBees <77853463+TheLordofBees@users.noreply.github.com>
2021-01-28 15:40:55 -0500

    Projects for App Development Class

---
+++ week_one/js/app.js
@@ -0,0 +1,54 @@
+function setup() {
+    var blue = "#69B4E4";
+    var startX = 100
+    var startY = 50
+ createCanvas(800,600);
+
+ background(105, 180, 228)
+
+ //Base of Submarine 
+ 
+ fill(153, 105, 228)
+ stroke(214, 105, 228)
+ strokeWeight (25)
+
+ rect(40, 150, 740, 200)
+ fill(153, 105, 228)
+rect(60, 150, 700, 200,)
+noStroke()
+
+//Windows
+
+ fill(blue)
+ stroke(105, 228, 214)
+ strokeWeight (25)
+ circle(140, 250, 50)
+ 
+ fill(blue)
+ stroke(105, 228, 214)
+ strokeWeight (25)
+ circle(410, 250, 50)
+
+ fill(blue)
+ stroke(105, 228, 214)
+ strokeWeight (25)
+ circle(680, 250, 50)
+
+ //Bottom Line 
+
+ stroke(2)
+ line(40, 350, 780, 350)
+
+//Submarine Top
+ stroke(228, 105, 180)
+ rect(600, 70, 20, 60)
+ 
+
+ rect(600, 70, 60, 20)
+
+stroke(228, 105, 118)
+fill(119, 147, 165)
+ circle(655, 80, 5)
+ 
+ //Will's Project One
+}
